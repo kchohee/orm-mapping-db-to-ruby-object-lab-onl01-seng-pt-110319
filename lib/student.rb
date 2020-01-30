f7226506c5e1ca30a2d61fed1fc09f0133e7b110
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 16:15:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,11 @@
 class Student
   attr_accessor :id, :name, :grade
   def self.new_from_db(row)
+    new_song = self.new  # self.new is the same as running Song.new
+    new_song.id = row[0]
+    new_song.name =  row[1]
+    new_song.length = row[2]
+    new_song 
   end
   def self.all
   end
