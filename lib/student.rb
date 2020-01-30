b54ee0cb9db409932634152480064f2a13fbc6ee
Marie (Cho) Hughes <kchohee0630@gmail.com>
2020-01-30 16:27:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,7 +14,6 @@ class Student
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-      end
     end
   end
   def self.find_by_name(name)
@@ -26,14 +25,13 @@ class Student
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
-      end.first
-    end
+    end.first
   end
   def self.all_students_in_grade_9
-    sql ==<<-SQL
+    sql = <<-SQL
       SELECT *
       FROM students
-      WHERE grade = 9 
+      WHERE grade = 9
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
@@ -83,7 +81,7 @@ class Student
   end
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
